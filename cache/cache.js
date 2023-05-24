78d0dc4823605ae08c05a04c470f66566da19cd1
Charlie <cprincewebdev@gmail.com>
2023-05-24 17:07:19 +0100

    Fix issues chaching images

--- cache/cache.js
+++ cache/cache.js
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 const fs = require('fs')
 const path = require('path')
 const matter = require('gray-matter')
@@ -5,7 +7,7 @@ const matter = require('gray-matter')
 function postData() {
   const postsDirectory = path.join(process.cwd(), '_posts')
   const fileNames = fs.readdirSync(postsDirectory)
-  const posts = fileNames.map((fileName: string) => {
+  const posts = fileNames.map((fileName) => {
     const id = fileName.replace(/\.mdx$/, '')
     const fullPath = path.join(postsDirectory, fileName)
     const fileContents = fs.readFileSync(fullPath, 'utf8')
@@ -24,9 +26,7 @@ try {
   fs.mkdirSync('cache')
 }
 
-fs.writeFile('cache/data.js', postData(), function (err: any) {
+fs.writeFile('cache/data.js', postData(), function (err) {
   if (err) return console.log(err);
   console.log('Posts cached.');
 })
-
-export {}
