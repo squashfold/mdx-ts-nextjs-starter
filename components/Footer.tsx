c004612064214ec4fa1b9437ce68f7d42547d5da
Charlie <cprincewebdev@gmail.com>
2023-05-26 23:17:07 +0100

    Update footer copy

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,7 +10,7 @@ const Footer: React.FC = () => {
         // Footer value
         <footer className={`${FooterStyles.footer}`}>
             <div className={`container ${FooterStyles['Footer__inner-wrap']}`}>
-                <span><strong>TypeScript MDX Blog Starter</strong> by SquashFold (Charlie Prince) &nbsp;
+                <span><strong>TypeScript MDX Blog Starter</strong> by SquashFold &nbsp;
                     <Link href="https://github.com/chprince/mdx-ts-nextjs-starter">
                         <a className="text-link text-link--bright">View on GitHub</a>
                     </Link>
