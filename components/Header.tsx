7b37a17adf0c51f37a95e89b0bf608a65fc9082d
Charlie <cprincewebdev@gmail.com>
2023-05-31 23:19:23 +0100

    Add basic mobile navigation

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 // Import the link props
+import { useState } from 'react'
 import Link from 'next/link';
 import Config from "../app.config"
 import HeaderStyles from '../styles/modules/Header.module.scss';
@@ -6,6 +7,8 @@ import NavigationStyles from '../styles/modules/Navigation.module.scss';
 
 // add the React Header Element
 const Header: React.FC = () => {
+    
+    const [navOpen, setNavOpen] = useState<boolean>(false)
 
     return (
         // header value
@@ -20,20 +23,22 @@ const Header: React.FC = () => {
                     <a className=""><strong>{Config.title}</strong></a>
                 </Link>
 
-                <nav className={`${NavigationStyles['main-nav']}`}>
+                <nav className={`${NavigationStyles['main-nav']} ${navOpen && NavigationStyles['_active']}`}>
                     <ul className={`${NavigationStyles['main-nav__list']}`}>
                         <li className={`${NavigationStyles['main-nav__item']}`}>
-                            <Link href="/about">
-                                <a className={`${NavigationStyles['main-nav__link']}`}>About</a>
+                            <Link href="/about" prefetch={true}>
+                                <a onClick={() => setNavOpen(false)} className={`${NavigationStyles['main-nav__link']}`}>About</a>
                             </Link>
                         </li>
                         <li className={`${NavigationStyles['main-nav__item']}`}>
-                            <Link href="/search">
-                                <a className={`${NavigationStyles['main-nav__link']}`}>Search</a>
+                            <Link href="/search" prefetch={true}>
+                                <a onClick={() => setNavOpen(false)} className={`${NavigationStyles['main-nav__link']}`}>Search</a>
                             </Link>
                         </li>
                     </ul>
                 </nav>
+
+                <button className={`${NavigationStyles['menu-toggle']} button button--white button--fill button--small`} onClick={() => setNavOpen(!navOpen)}>{!navOpen ? 'Open menu' : 'Close menu'}</button>
             </div>
         </header>
     )
